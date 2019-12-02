daa85992644f383be034c39b00684a814232252c
s. kusuhara <49839611+mikana0918@users.noreply.github.com>
2019-12-02 13:22:01 +0900

    Update useful-functions-date.js
    add dateformat YYYY/MM/DD HH:MM

--- useful-functions-date.js
+++ useful-functions-date.js
@@ -1,4 +1,4 @@
-//using example: alert(formatDate('Sun May 11,2014'));
+//using example: formatDate('Sun May 11,2014');
 //output: 2014-05-11
 function formatDate(date) {
     var d = new Date(date),
@@ -12,6 +12,23 @@ function formatDate(date) {
         day = '0' + day;
 
     return [year, month, day].join('-');
+}
+
+//FormatDate as YYYY/MM/DD HH:MM
+function formatDate(date) {
+    var d = new Date(date),
+        minutes = d.getMinutes()
+        hours = d.getHours();
+        month = '' + (d.getMonth() + 1),
+        day = '' + d.getDate(),
+        year = d.getFullYear();
+
+    if (month.length < 2) 
+        month = '0' + month;
+    if (day.length < 2) 
+        day = '0' + day;
+
+    return [year, month, day].join('-')+' '+[hours, minutes].join(':');
 }
 
 /**
