74c825be241424bce7501813f7a7c173fbd8c8db
楠原彰悟 <s.kusuhara@i3design.co.jp>
2019-12-02 12:23:19 +0900

    my first commit

---
+++ useful-functions-date.js
@@ -0,0 +1,15 @@
+//using example: alert(formatDate('Sun May 11,2014'));
+//output: 2014-05-11
+function formatDate(date) {
+    var d = new Date(date),
+        month = '' + (d.getMonth() + 1),
+        day = '' + d.getDate(),
+        year = d.getFullYear();
+
+    if (month.length < 2) 
+        month = '0' + month;
+    if (day.length < 2) 
+        day = '0' + day;
+
+    return [year, month, day].join('-');
+}
